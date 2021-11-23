370bc2f92b68afaf81164bd12c3b2877f5e820a9
mocmoc15 <aiai2.0831@gmail.com>
2021-11-23 14:35:25 +0900

    マイグレーションファイルの修正（profile削除）

--- db/migrate/20211113134429_devise_create_users.rb
+++ db/migrate/20211113134429_devise_create_users.rb
@@ -7,7 +7,6 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
       t.string :name
-      t.text   :profile
       t.text   :occupation
       t.text   :position
 
