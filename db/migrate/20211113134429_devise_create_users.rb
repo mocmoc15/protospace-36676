999f74c7500013c4e1c497d1bec15b8f4b520d65
mocmoc15 <aiai2.0831@gmail.com>
2021-11-24 12:41:27 +0900

    indexファイル修正（、)

--- db/migrate/20211113134429_devise_create_users.rb
+++ db/migrate/20211113134429_devise_create_users.rb
@@ -11,7 +11,6 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       t.text   :occupation
       t.text   :position
 
-
       ## Recoverable
       t.string   :reset_password_token
       t.datetime :reset_password_sent_at
