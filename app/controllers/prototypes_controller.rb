5ba707a39a234a4f54b038523e6b5435a832b3d2
mocmoc15 <aiai2.0831@gmail.com>
2021-11-24 22:22:32 +0900

    詳細ページへの遷移と編集ページへの制御

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -55,7 +55,8 @@ def prototype_params
 end
 
 def move_to_index
-  unless user_signed_in?
+  @prototype = Prototype.find(params[:id])
+  unless user_signed_in? && current_user.id == @prototype.user_id
     redirect_to action: :index
   end
 end
